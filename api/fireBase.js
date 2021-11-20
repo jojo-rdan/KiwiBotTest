e6edd8eac1440d708f204c68aa7fad83ac3d1521
Jordan <jordan.olaves91@gmail.com>
2021-11-20 09:06:15 -0500

    securityChanges

--- api/fireBase.js
+++ api/fireBase.js
@@ -2,7 +2,7 @@
 
 //Permisos de Admin para firebase.
 var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
+var serviceAccount = require("../../serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- api/index.js
+++ api/index.js
@@ -3,13 +3,19 @@ const {Deliveries, Bots} = require('./fireBase')
 const app = express();
 const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
-app.use(express.json());
 
 //Acá seteo los headers
+app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended : true, limit: '50mb'}));
 
-//Post para crear Deliveries
+//Cuando tengo un poco más de tiempo modularizo las rutas en la carpeta src
+//en donde divido las rutas como componentes funcionales y las llamo sobre un index
+//con esto es más fácil localizar un error en cierta porción de código.
+//también utilizo varianbles de ambiente (dotenv) para guardar información sensible.
+//cabe destacar que las rutas tienen un control de errores que blindan el back para mayor seguridad.
+
+//Ruta para crear Deliveries
 app.post('/delivery', async (req, res) => {
         let {
             pickup: {
@@ -46,6 +52,8 @@ app.post('/delivery', async (req, res) => {
             console.log(err)
         }
 })
+
+//Ruta para crear bots
 app.post('/bots', async(req, res) => {
     let {
         location: {
@@ -73,6 +81,8 @@ app.post('/bots', async(req, res) => {
             console.log(err);
         }
 })
+
+//Ruta para obtener todos los deliveries en caso de que no pases un id por params
 app.get('/delivery', async(req, res) => {
         try{
             const delivery = await Deliveries.get();
@@ -97,6 +107,8 @@ app.get('/delivery', async(req, res) => {
             console.log(err);
         }
 })
+
+//Ruta para obtener un delivery específico
 app.get('/delivery/:id', async(req, res) => {
         const {id} = req.params;
         try{
@@ -107,6 +119,11 @@ app.get('/delivery/:id', async(req, res) => {
             console.log(err);
         }
 })
+
+//Ruta referencial para cambiar el estado de un pedido y a la vez modificar el status de un bot
+//los objetos states y statusBots dentro de la ruta funcionan como a modo de "toggles" debido a que
+//tanto el punto 7 como 8 de la prueba técnica están relacionadas ya que asignar un bot a un delivery
+//modifica ambas disponibilidades tanto del bot como del delivery.
 app.put('/delivery/:id', async(req, res) => {
     const {id} = req.params;
     const {action} = req.query;
